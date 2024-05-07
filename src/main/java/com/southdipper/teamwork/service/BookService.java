2e747207e5d12d439793a00c36204c69a3e1c3dd
shishuiliuqing <1410919730@qq.com>
2024-05-07 14:34:41 +0800

    BookService添加search方法（按需求查找待售书籍）

--- src/main/java/com/southdipper/teamwork/service/BookService.java
+++ src/main/java/com/southdipper/teamwork/service/BookService.java
@@ -1,9 +1,15 @@
 package com.southdipper.teamwork.service;
 
 import com.southdipper.teamwork.pojo.Book;
+import com.southdipper.teamwork.pojo.SelectRequest;
+
+import java.util.List;
 
 public interface BookService {
 
     //添加书籍
     void add(Book book);
+
+    //按要求查找别人的书籍
+    List<Book> search(SelectRequest selectRequest);
 }
--- src/main/java/com/southdipper/teamwork/service/impl/BookServiceImpl.java
+++ src/main/java/com/southdipper/teamwork/service/impl/BookServiceImpl.java
@@ -2,9 +2,12 @@ package com.southdipper.teamwork.service.impl;
 
 import com.southdipper.teamwork.mapper.BookMapper;
 import com.southdipper.teamwork.pojo.Book;
+import com.southdipper.teamwork.pojo.SelectRequest;
 import com.southdipper.teamwork.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -15,5 +18,11 @@ public class BookServiceImpl implements BookService {
     @Override
     public void add(Book book) {
         bookMapper.add(book);
+    }
+
+    @Override
+    public List<Book> search(SelectRequest selectRequest) {
+        List<Book> bookList = bookMapper.search(selectRequest);
+        return bookList;
     }
 }
