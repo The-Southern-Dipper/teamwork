a249cabcc4167f84e4a7b33c876a449664b54176
shishuiliuqing <1410919730@qq.com>
2024-05-07 15:24:05 +0800

    提供delete接口，可通过书籍ID删除书籍

--- src/main/java/com/southdipper/teamwork/controller/BookController.java
+++ src/main/java/com/southdipper/teamwork/controller/BookController.java
@@ -5,10 +5,7 @@ import com.southdipper.teamwork.pojo.Result;
 import com.southdipper.teamwork.pojo.SelectRequest;
 import com.southdipper.teamwork.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,5 +35,12 @@ public class BookController {
     public Result searchFromId(Integer sellerId) {
         List<Book> bookList = bookService.searchFromId(sellerId);
         return Result.success(bookList);
+    }
+
+    //删除待售书籍(按书籍ID进行删除)
+    @PutMapping("/delete")
+    public Result delete(Integer id) {
+        bookService.delete(id);
+        return Result.success();
     }
 }
